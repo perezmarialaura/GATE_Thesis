34090d9c6d595e70ec8b2bffa6a986775e56f5f2
Maria Laura Perez <33493064+perezmarialaura@users.noreply.github.com>
2020-08-20 16:04:06 -0500

    Fit with 360º

--- Repository/5-RadICAL/output/RadCurvesFit.py
+++ Repository/5-RadICAL/output/RadCurvesFit.py
@@ -8,11 +8,11 @@ from scipy.signal import argrelextrema
 
 
 plt.rc('font', family='Helvetica')
-x = np.linspace(0, 180, 180)
+x = np.linspace(0, 360, 943) # The smallest angle which translates to 1 mm distance is 0.38 degrees
 
 #-------------FUNCTIONS----------------------------
-def sinusoidal(x, amp, omega, phi, d):
-    return amp*np.sin(omega*x*np.pi/180 + phi) + d
+def sinusoidal(x_vals, amp, phi, d):
+    return amp * np.abs( np.sin(np.pi * (x_vals-phi)/180) ) + d
     
     
 def normalize(v):
@@ -21,28 +21,30 @@ def normalize(v):
         return v
     return v / norm
 
+
 def y_func(bins, vals, initial_guess):
     params, _ = optimize.curve_fit(sinusoidal, bins, vals, p0=initial_guess, maxfev=5000)
-    y = sinusoidal(x, params[0], params[1], params[2], params[3])
-    maxangle = np.round(x[np.argmax(y)], 2)
-    return y, maxangle
+    y = sinusoidal(x, params[0], params[1], params[2])
+    local_min = argrelextrema(y, np.less)[0]
+    min_angles = np.round(x[local_min], 2)
+    #minangle = np.round(x[np.argmin(y)], 2)
+    return y, min_angles
     
-def y_func_poly(bins, vals):
-    p = np.polyfit(bins, vals, 5)
-    y = p[0]*x**5 + p[1] *x**4 + p[2]*x**3 + p[3]*x**2 + p[4]*x + p[5]
-    #local_max = argrelextrema(y, np.greater)
-    #local_min = argrelextrema(y, np.less)
-    maxangle = np.round(x[np.argmax(y)], 2)
-    return y, maxangle
+    
+def error(theo, exp):
+    return np.round(np.abs((theo-exp)), 2)
+    
+    
+def distance_dif(angle1, angle2):
+    x1 = np.abs(np.tan((angle1-90)*np.pi/180))
+    x2 = np.abs(np.tan((angle2-90)*np.pi/180))
+    return np.round(15*np.abs(x1-x2), 2)
+    
 
 #-----------------DATA------------------------------
-#Filtered data for gammas
+#Data for gammas
 bins_gpr, vals_gpr =np.genfromtxt("S3A_Gammas_RadCurve.txt", unpack=True)
 bins_gcr, vals_gcr =np.genfromtxt("S3B_Gammas_RadCurve.txt", unpack=True)
-
-#Unfiltered data for gammas
-bins_gpr_nf, vals_gpr_nf = np.genfromtxt("S3A_Gammas_RadCurve_NF.txt", unpack=True)
-bins_gcr_nf, vals_gcr_nf = np.genfromtxt("S3B_Gammas_RadCurve_NF.txt", unpack=True)
 
 #Filtered data for neutrons
 bins_npr, vals_npr =np.genfromtxt("S3A_Neutrons_RadCurve.txt", unpack=True)
@@ -53,16 +55,21 @@ bins_npr_nf, vals_npr_nf = np.genfromtxt("S3A_Neutrons_RadCurve_NF.txt", unpack=
 bins_ncr_nf, vals_ncr_nf = np.genfromtxt("S3B_Neutrons_RadCurve_NF.txt", unpack=True)
 
 #Fits
-y_gpr_nf, maxangle_gpr_nf = y_func(bins_gpr_nf, vals_gpr_nf, [450, 2, 2, 0.07])
-y_gpr, maxangle_gpr = y_func(bins_gpr, vals_gpr,[100, 2, 2, 0.07])
-y_gcr_nf, maxangle_gcr_nf = y_func(bins_gcr_nf, vals_gcr_nf, [700, 2, 1, 0.07])
-y_gcr, maxangle_gcr = y_func(bins_gcr, vals_gcr, [200, 1, 0, 400])
+y_gpr, mins_gpr = y_func(bins_gpr, vals_gpr,[250, 20, 400])
+y_gcr, mins_gcr = y_func(bins_gcr, vals_gcr, [400, 0, 600])
+y_npr_nf, mins_npr_nf = y_func(bins_npr_nf, vals_npr_nf, [115, 0, 60])
+y_npr, mins_npr = y_func(bins_npr, vals_npr, [45, -20, 10])
+y_ncr_nf, mins_ncr_nf = y_func(bins_ncr_nf, vals_ncr_nf, [100, 0, 50])
+y_ncr, mins_ncr = y_func(bins_ncr, vals_ncr, [20, 0, 20])
 
+exp_angles = np.array([mins_gpr[1]-90, mins_gcr[0]-90, mins_npr_nf[0]-90, mins_npr[0]-90, mins_ncr[1]-90, mins_ncr_nf[0]-90])
 
-y_npr_nf, maxangle_npr_nf = y_func(bins_npr_nf, vals_npr_nf, [110, 1, 0, 60])
-y_npr, maxangle_npr = y_func_poly(bins_npr, vals_npr)
-y_ncr_nf, maxangle_ncr_nf = y_func(bins_ncr_nf, vals_ncr_nf, [80, 1, 0, 60])
-y_ncr, maxangle_ncr = y_func_poly(bins_ncr, vals_ncr)
+theo_angles = np.array([99.83, 95.71, 80.54, 80.54, 71.56, 71.56])
+angle_errors = error(theo_angles, exp_angles)
+dist_errors = distance_dif(theo_angles, exp_angles)
+
+print('Angle errors: ', angle_errors)
+print('Distance errors: ', dist_errors)
 
 #--------------------GAMMA PLOT-------------------------------------
 fig, ax1 = plt.subplots(1, 2, figsize=(22,10))
@@ -70,16 +77,12 @@ fig.subplots_adjust(left=0.05, bottom=0.1, right=0.95, top=0.9, wspace=0.25, hsp
 plt.suptitle('RadICAL curves - Gammas', fontsize=30, fontweight='bold')
 
 ax1[0].set_title('Point source', fontsize=26)
-ax1[0].scatter(bins_gpr_nf, vals_gpr_nf, label='Unfiltered data', color='deepskyblue', s=15)
-ax1[0].scatter(bins_gpr, vals_gpr, label='Filtered data', color='royalblue', s=15)
-ax1[0].plot(x, y_gpr_nf, linewidth=2, label='Fit - Max pos = '+str(maxangle_gpr_nf)+'º',  color='deepskyblue')
-ax1[0].plot(x, y_gpr, linewidth=2, label='Fit - Max pos = '+str(maxangle_gpr)+'º', color='royalblue')
+ax1[0].scatter(bins_gpr, vals_gpr, label='Data', color='royalblue', s=15)
+ax1[0].plot(x, y_gpr, linewidth=2, label='Fit: min at '+str(mins_gpr[0])+', '+str(mins_gpr[1])+'º', color='royalblue')#- Max pos = '+str(maxangle_gpr)+'º')
 
 ax1[1].set_title('Continuous source', fontsize=26)
-ax1[1].scatter(bins_gcr_nf, vals_gcr_nf, label='Unfiltered data', color='yellowgreen', s=15)
-ax1[1].scatter(bins_gcr, vals_gcr, label='Filtered data', color='olivedrab', s=15)
-ax1[1].plot(x, y_gcr_nf, linewidth=2, label='Fit - Max pos = '+str(maxangle_gcr_nf)+'º',  color='yellowgreen')
-ax1[1].plot(x, y_gcr, linewidth=2, label='Fit - Max pos = '+str(maxangle_gcr)+'º', color='olivedrab')
+ax1[1].scatter(bins_gcr, vals_gcr, label='Data', color='olivedrab', s=15)
+ax1[1].plot(x, y_gcr, linewidth=2, label='Fit: min at '+str(mins_gcr[0])+', '+str(mins_gcr[1])+'º', color='olivedrab') #- Max pos = '+str(maxangle_gcr)+'º')
 
 ax1[0].set_ylabel('N events', fontweight='bold', fontsize=24)
 ax1[0].tick_params(axis='both', which='major', labelsize=24, direction='in', length=10)
@@ -87,8 +90,8 @@ ax1[0].tick_params(axis='both', which='minor', direction='in', length=5)
 ax1[0].xaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[0].yaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[0].set_xlabel('Rotation angle (deg)', fontweight='bold', fontsize=24)
-ax1[0].set_xlim(0, 180)
-ax1[0].set_ylim(0, 800)
+ax1[0].set_xlim(0, 360)
+ax1[0].set_ylim(300, 800)
 ax1[0].legend(fontsize=24, ncol=2)
 
 ax1[1].set_ylabel('N events', fontweight='bold', fontsize=24)
@@ -97,8 +100,8 @@ ax1[1].tick_params(axis='both', which='minor', direction='in', length=5)
 ax1[1].xaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[1].yaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[1].set_xlabel('Rotation angle (deg)', fontweight='bold', fontsize=24)
-ax1[1].set_xlim(0, 180)
-ax1[1].set_ylim(0, 1200)
+ax1[1].set_xlim(0, 360)
+ax1[1].set_ylim(500, 1200)
 ax1[1].legend(fontsize=24, ncol=2)
 plt.savefig('S3_Gammas.png')
 #plt.show()
@@ -109,16 +112,16 @@ fig.subplots_adjust(left=0.05, bottom=0.1, right=0.95, top=0.9, wspace=0.25, hsp
 plt.suptitle('RadICAL curves - Neutrons', fontsize=30, fontweight='bold')
 
 ax1[0].set_title('Point source', fontsize=26)
-ax1[0].scatter(bins_npr_nf, vals_npr_nf, label='Unfiltered data', color='deepskyblue', s=15)
-ax1[0].scatter(bins_npr, vals_npr, label='Filtered data', color='royalblue', s=15)
-ax1[0].plot(x, y_npr_nf, linewidth=2, label='Fit - Max pos = '+str(maxangle_npr_nf)+'º',  color='deepskyblue')
-ax1[0].plot(x, y_npr, linewidth=2, label='Fit', color='royalblue')
+ax1[0].scatter(bins_npr_nf, vals_npr_nf, label='Data (Unfilt)', color='deepskyblue', s=15)
+ax1[0].scatter(bins_npr, vals_npr, label='Data (Filt)', color='royalblue', s=15)
+ax1[0].plot(x, y_npr_nf, linewidth=2, label='Fit: min at '+str(mins_npr_nf[0])+', '+str(mins_npr_nf[1])+'º', color='deepskyblue')
+ax1[0].plot(x, y_npr, linewidth=2, label='Fit: min at '+str(mins_npr[0])+', '+str(mins_npr[1])+'º', color='royalblue')
 
 ax1[1].set_title('Continuous source', fontsize=26)
-ax1[1].scatter(bins_ncr_nf, vals_ncr_nf, label='Unfiltered data', color='yellowgreen', s=15)
-ax1[1].scatter(bins_ncr, vals_ncr, label='Filtered data', color='olivedrab', s=15)
-ax1[1].plot(x, y_ncr_nf, linewidth=2, label='Fit - Max pos = '+str(maxangle_ncr_nf)+'º',  color='yellowgreen')
-ax1[1].plot(x, y_ncr, linewidth=2, label='Fit', color='olivedrab')
+ax1[1].scatter(bins_ncr_nf, vals_ncr_nf, label='Data (Unfilt)', color='yellowgreen', s=15)
+ax1[1].scatter(bins_ncr, vals_ncr, label='Data (Filt)', color='olivedrab', s=15)
+ax1[1].plot(x, y_ncr_nf, linewidth=2, label='Fit: min at '+str(mins_ncr_nf[0])+'º',  color='yellowgreen')
+ax1[1].plot(x, y_ncr, linewidth=2, label='Fit: min at '+str(mins_ncr[0])+', '+str(mins_ncr[1])+'º', color='olivedrab')
 
 ax1[0].set_ylabel('N events', fontweight='bold', fontsize=24)
 ax1[0].tick_params(axis='both', which='major', labelsize=24, direction='in', length=10)
@@ -126,8 +129,8 @@ ax1[0].tick_params(axis='both', which='minor', direction='in', length=5)
 ax1[0].xaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[0].yaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[0].set_xlabel('Rotation angle (deg)', fontweight='bold', fontsize=24)
-ax1[0].set_xlim(0, 180)
-ax1[0].set_ylim(0, 180)
+ax1[0].set_xlim(0, 360)
+ax1[0].set_ylim(0, 200)
 ax1[0].legend(fontsize=24, ncol=2)
 
 ax1[1].set_ylabel('N events', fontweight='bold', fontsize=24)
@@ -136,7 +139,7 @@ ax1[1].tick_params(axis='both', which='minor', direction='in', length=5)
 ax1[1].xaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[1].yaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax1[1].set_xlabel('Rotation angle (deg)', fontweight='bold', fontsize=24)
-ax1[1].set_xlim(0, 180)
-ax1[1].set_ylim(0, 160)
+ax1[1].set_xlim(0, 360)
+ax1[1].set_ylim(0, 200)
 ax1[1].legend(fontsize=24, ncol=2)
 plt.savefig('S3_Neutrons.png')
